a6fa0e0c50624dac9bf144cbc0d87e0e2d901df7
Botir Khaltaev <btrghstk@gmail.com>
2024-11-06 23:23:30 +0000

    Feature/notifications bell dropdown (#39)
    * Feat: Add notifications dropdown menu and replace sidebar
    
    * Fix: Remove unused components in dashboard
    
    * FIx: Make drop down menu rtl

--- student-copilot-frontend/src/components/custom/dashboard/user-notifications.tsx
+++ student-copilot-frontend/src/components/custom/dashboard/user-notifications.tsx
@@ -1,32 +1,26 @@
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { Doc } from "convex/_generated/dataModel";
-import { StaticList } from "../static-list";
 import { Button } from "@/components/ui/button";
-import { Id } from "convex/_generated/dataModel";
+import { Doc, Id } from "convex/_generated/dataModel";
 import { Trash2Icon } from "lucide-react";
 interface NotificationItemProps {
-  message: string;
-  type: string;
-  createdAt: string;
-  id: string;
+  notification: Doc<"notifications">;
 }
 
 const NotificationItem: React.FC<NotificationItemProps> = ({
-  message,
-  type,
-  createdAt,
-  id,
+  notification,
 }) => {
   const deleteNotification = useMutation(api.notifications.deleteNotification);
   return (
-    <div className="bg-card text-card-foreground shadow-md rounded-lg p-4 w-full max-w-md">
+    <div className="bg-card text-card-foreground  p-4 w-full max-w-md">
       <div className="flex items-center justify-between">
-        <p className="text-foreground">{message}</p>
+        <p className="text-foreground">{notification.message}</p>
 
         <Button
           onClick={() =>
-            deleteNotification({ notificationId: id as Id<"notifications"> })
+            deleteNotification({
+              notificationId: notification._id as Id<"notifications">,
+            })
           }
           variant={"ghost"}
           className="text-destructive"
@@ -35,32 +29,11 @@ const NotificationItem: React.FC<NotificationItemProps> = ({
         </Button>
       </div>
       <div className="flex justify-between mt-2 text-sm text-muted-foreground">
-        <span>{type}</span>
-        <span>{new Date(createdAt).toLocaleString()}</span>
+        <span>{notification.type}</span>
+        <span>{new Date(notification.createdAt).toLocaleString()}</span>
       </div>
     </div>
   );
 };
 
-export const UserNotifications: React.FC = () => {
-  const userNotifications =
-    useQuery(api.notifications.getUserNotifications, {
-      limit: 6,
-    }) || [];
-
-  return (
-    <StaticList className="w-full max-w-md lg:max-w-none space-y-4">
-      {userNotifications.map((notification: Doc<"notifications">) => (
-        <NotificationItem
-          key={notification._id}
-          id={notification._id}
-          message={notification.message}
-          type={notification.type}
-          createdAt={notification.createdAt}
-        />
-      ))}
-    </StaticList>
-  );
-};
-
-export default UserNotifications;
+export default NotificationItem;
--- student-copilot-frontend/src/layouts/dashboard-layout.tsx
+++ student-copilot-frontend/src/layouts/dashboard-layout.tsx
@@ -1,85 +1,18 @@
-import { useState } from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 import { Outlet } from "react-router-dom";
-import { useConvexAuth } from "convex/react";
-import { UserButton } from "@clerk/clerk-react";
-import { IconHome, IconSettings } from "@tabler/icons-react";
-import { Sidebar, SidebarBody, SidebarLink } from "@/components/custom/sidebar";
-import LoadingPage from "@/components/custom/loading";
-import Logo from "@/components/custom/logo";
-import TreeView from "@/components/ui/tree-view";
-import { Separator } from "@/components/ui/separator";
-import UpgradePlanModal from "@/components/custom/dashboard/upgrade-plan-modal";
+import { Authenticated } from "convex/react";
 
 export default function DashboardLayout() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const { isLoading, isAuthenticated } = useConvexAuth();
-
-  if (isLoading) {
-    return <LoadingPage />;
-  }
-
-  if (!isAuthenticated && !isLoading) {
-    console.log("Not authenticated");
-  }
-
   return (
-    <div className="flex w-full">
-      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen}>
-        {/* Sidebar layout with scrollable top and sticky bottom */}
-        <SidebarBody className="flex flex-col justify-between h-full">
-          {/* Scrollable section */}
-          <div className="flex-grow overflow-y-auto space-y-4">
-            <SidebarLink
-              link={{
-                label: "",
-                href: "/dashboard/home",
-                icon: <Logo size="lg" />,
-              }}
-            />
-
-            <Separator />
-
-            <SidebarLink
-              link={{
-                label: "Home",
-                href: "/dashboard/home",
-                icon: <IconHome size={24} />,
-              }}
-            />
-            <TreeView isSidebarOpen={sidebarOpen} />
-          </div>
-
-          {/* Sticky bottom section */}
-          <div className="flex-shrink-0 sticky bottom-0 ">
-            <Separator className="mb-2" />
-
-            {sidebarOpen && (
-              <div className="w-full p-2">
-                <UpgradePlanModal />
-
-              </div>
-            )}
-
-            <div className="flex justify-between items-center w-full gap-4 p-2">
-              <SidebarLink
-                link={{
-                  label: "Settings",
-                  href: "/dashboard/settings",
-                  icon: <IconSettings size={24} />,
-                }}
-              />
-              <UserButton />
-            </div>
-          </div>
-        </SidebarBody>
-      </Sidebar>
-
-      <div className="flex flex-col flex-grow ">
-        <div className="flex justify-end items-center p-4 md:hidden">
-          <UserButton />
-        </div>
-        <Outlet />
-      </div>
-    </div>
+    <Authenticated>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="min-h-full min-w-full">
+          <SidebarTrigger />
+          <Outlet />
+        </main>
+      </SidebarProvider>
+    </Authenticated>
   );
 }
