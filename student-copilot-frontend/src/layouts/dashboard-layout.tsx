da05501e69ce6805b677dec0d9d0b2d48b274a04
Botir Khaltaev <btrghstk@gmail.com>
2024-11-07 11:51:15 +0000

    Fix: Dashboard overflow with sidebar trigger (#42)
    * Feat: Add activity tracker for users
    
    * Fix: Fix tsc issues
    
    * Fix: Do a clean install as package.lock changed
    
    * Fix: Add missing modules
    
    * Fix: Downgrade canvas version
    
    * Fix: Reinstall packages
    
    * Fix: REmove pdf libraries
    
    * Fix: Remove canvas dependency
    
    * Fix: Add react pdf to text
    
    * Fix: make the side bar trigger fixed on the screen

--- student-copilot-frontend/src/layouts/dashboard-layout.tsx
+++ student-copilot-frontend/src/layouts/dashboard-layout.tsx
@@ -8,8 +8,8 @@ export default function DashboardLayout() {
     <Authenticated>
       <SidebarProvider>
         <AppSidebar />
-        <main className="min-h-full min-w-full">
-          <SidebarTrigger className="m-2" />
+        <main className="max-h-full max-w-full">
+          <SidebarTrigger className="fixed" />
           <Outlet />
         </main>
       </SidebarProvider>
