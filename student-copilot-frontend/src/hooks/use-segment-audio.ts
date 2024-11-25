cd30b51522239dedd332362ca484693b933eff1a
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-11-25 17:13:59 +0000

    Fix: Add https bit

--- student-copilot-frontend/src/hooks/use-segment-audio.ts
+++ student-copilot-frontend/src/hooks/use-segment-audio.ts
@@ -51,7 +51,7 @@ const useSegmentAudio = () => {
       console.log("VITE API URL", import.meta.env.VITE_API_URL);
 
       const response = await axios.post<RawAudioSegment[]>(
-        `c-api-production.up.railway.app/Audio/segment`,
+        `https://c-api-production.up.railway.app/Audio/segment`,
         formData,
         {
           params: { maxTokensPerSegment: MAX_TOKENS_PER_SEGMENT },
