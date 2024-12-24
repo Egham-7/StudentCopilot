49b6b110fe98972da88837fe1b80fda4efe418f6
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2024-12-24 16:28:11 +0000

    modified sign in and sign up button

--- student-copilot-frontend/src/components/custom/orbiting-circles.tsx
+++ student-copilot-frontend/src/components/custom/orbiting-circles.tsx
@@ -4,7 +4,7 @@ export function OrbitingCirclesLandingPage() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
-        Circles
+        In One Place
       </span>
 
       {/* Inner Circles */}
@@ -174,4 +174,3 @@ const Icons = {
     </svg>
   ),
 };
-
